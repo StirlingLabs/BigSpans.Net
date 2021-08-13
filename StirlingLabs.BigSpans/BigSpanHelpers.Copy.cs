832dce3a91b0e17b4083cd535b5a73585ab14ab8
Tyler Young <tyler.young@stirlinglabs.com>
2021-08-13 19:05:52 -0400

    clean up BigSpanHelpers.Copy

--- StirlingLabs.BigSpans/BigSpanHelpers.Copy.cs
+++ StirlingLabs.BigSpans/BigSpanHelpers.Copy.cs
@@ -7,32 +7,7 @@ namespace StirlingLabs.Utilities
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe void Copy(byte* destinationPointer, byte* sourcePointer, nuint length)
-        {
-            Buffer.MemoryCopy(sourcePointer, destinationPointer, length, length);
-            /*
-            var dist = (nuint)(destinationPointer - sourcePointer);
-            if (dist < length)
-            {
-                Buffer.MemoryCopy(sourcePointer, destinationPointer, length, length);
-                return;
-            }
-
-            var l = length;
-            ref var dstRef = ref Unsafe.AsRef<byte>(destinationPointer);
-            ref var srcRef = ref Unsafe.AsRef<byte>(sourcePointer);
-            while (l >= uint.MaxValue)
-            {
-                Unsafe.CopyBlockUnaligned(ref dstRef, ref srcRef, uint.MaxValue);
-                // ReSharper disable RedundantOverflowCheckingContext // CS8778
-                dstRef = ref Unsafe.AddByteOffset(ref dstRef, unchecked((nint)uint.MaxValue));
-                srcRef = ref Unsafe.AddByteOffset(ref srcRef, unchecked((nint)uint.MaxValue));
-                // ReSharper restore RedundantOverflowCheckingContext
-                l -= uint.MaxValue;
-            }
-            if (l > 0)
-                Unsafe.CopyBlockUnaligned(ref dstRef, ref srcRef, (uint)l);
-            */
-        }
+            => Buffer.MemoryCopy(sourcePointer, destinationPointer, length, length);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe void Copy(void* destinationPointer, void* sourcePointer, nuint length)
